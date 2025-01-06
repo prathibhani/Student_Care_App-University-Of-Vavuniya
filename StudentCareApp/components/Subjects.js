63ec87c84047e5272f9da0625cb337b160be0130
prathibhani <139643234+prathibhani@users.noreply.github.com>
2025-01-07 00:51:21 +0530

    styles add for course

--- StudentCareApp/components/Subjects.js
+++ StudentCareApp/components/Subjects.js
@@ -75,3 +75,69 @@ export default function Subjects() {
 </PaperProvider>
     );
 }
+const styles = StyleSheet.create({
+    container: {
+      padding: 20,
+    },
+    headerSubject: {
+      alignItems: "center",
+      marginBottom: 20,
+    },
+    headerTextsubject: {
+      fontSize: 24,
+      fontWeight: "bold",
+    },
+    inputContainer: {
+      marginBottom: 20,
+    },
+    input: {
+      marginBottom: 10,
+    },
+    button: {
+      marginTop: 10,
+    },
+    divider: {
+      marginVertical: 20,
+    },
+    courseListContainer: {
+      marginBottom: 20,
+    },
+    courseListTitle: {
+      fontSize: 18,
+      fontWeight: "bold",
+      marginBottom: 10,
+    },
+    averageContainer: {
+      alignItems: "center",
+      marginTop: 20,
+    },
+    averageText: {
+      fontSize: 18,
+      fontWeight: "bold",
+    },
+    header: {
+      width: "100%",
+      height: 100,
+      justifyContent: "center",
+      alignItems: "center",
+      backgroundColor: "#8b008b",
+    },
+    headerText: {
+      fontSize: 24,
+      color: "#fff",
+      backgroundColor: "#8b008b",
+      fontWeight: "bold",
+    },
+    image: {
+      width: "60%",
+      height: 100,
+      resizeMode: "contain",
+      marginTop: 5,
+    },
+    imagecontain: { alignItems: "center" },
+    loginText: {
+      fontSize: 40,
+      fontWeight: "bold",
+    },
+  });
+  
