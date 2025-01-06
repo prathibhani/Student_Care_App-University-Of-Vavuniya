94c0674459f9390304339d60c3b574459c06307e
prathibhani <139643234+prathibhani@users.noreply.github.com>
2025-01-06 23:11:57 +0530

    create the header and added the logo to header

--- StudentCareApp/components/Login.js
+++ StudentCareApp/components/Login.js
@@ -7,7 +7,15 @@ export default function Login(){
     return(
         <PaperProvider>
 <ScrollView>
-
+<View style={styles.header}>
+          <Text style={styles.headerText}>UoV Student Care</Text>
+        </View>
+        <View style={styles.imagecontain}>
+          <Image
+            source={require("../assets/uovlogo.png")}
+            style={styles.image}
+          />
+        </View>
 </ScrollView>
 
             </PaperProvider>
