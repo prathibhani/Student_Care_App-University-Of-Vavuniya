b026bc1bb03bdee23d316ab6a25a59053626f797
prathibhani <139643234+prathibhani@users.noreply.github.com>
2025-01-07 00:00:43 +0530

    Add view styles in header and added the logo in header

--- StudentCareApp/components/Profile.js
+++ StudentCareApp/components/Profile.js
@@ -2,7 +2,15 @@ export default  function Profile ({ route }){
     return(
 <PaperProvider>
 <ScrollView>
-    
+<View style={styles.header}>
+          <Text style={styles.headerText}>UoV Student Care</Text>
+        </View>
+        <View style={styles.imagecontain}>
+          <Image
+            source={require("../assets/uovlogo.png")}
+            style={styles.image}
+          />
+        </View>
 </ScrollView>
 </PaperProvider>
 
