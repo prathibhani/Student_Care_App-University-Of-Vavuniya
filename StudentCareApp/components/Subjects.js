b8aadf063719a89b8813a7fc829c9c65c35d82dd
prathibhani <139643234+prathibhani@users.noreply.github.com>
2025-01-07 00:45:38 +0530

    add the textinput for subject and marks and add a button Add subject and marks

--- StudentCareApp/components/Subjects.js
+++ StudentCareApp/components/Subjects.js
@@ -12,5 +12,32 @@ export default function Subjects() {
             source={require("../assets/uovlogo.png")}
             style={styles.image}
           />
+          </View>
+        <View style={styles.headerSubject}>
+          <Text style={styles.headerTextsubject}>Computer Science</Text>
+        </View>
+
+        <View style={styles.inputContainer}>
+          <TextInput
+            label="Subject"
+            value={subject}
+            onChangeText={setSubject}
+            style={styles.input}
+          />
+          <TextInput
+            label="Marks"
+            value={marks}
+            keyboardType="numeric"
+            onChangeText={setMarks}
+            style={styles.input}
+          />
+          <Button
+            mode="contained"
+            onPress={handleAddCourse}
+            style={styles.button}
+          >
+            Add Subject and Marks
+          </Button>
+        </View>
     );
 }
