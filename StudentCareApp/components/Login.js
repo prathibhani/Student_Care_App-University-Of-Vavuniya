9de39f5678ecfd12a264147361369c640337aa2c
prathibhani <139643234+prathibhani@users.noreply.github.com>
2025-01-06 23:10:39 +0530

    created ScrollView component in Login

--- StudentCareApp/components/Login.js
+++ StudentCareApp/components/Login.js
@@ -1,11 +1,15 @@
+import {ScrollView} from "react-native";
+import {PaperProvider} from "react-native-paper";
 import { students } from "../data/StudentsDb";
 import { useNavigation } from "@react-navigation/native";
 
 export default function Login(){
     return(
         <PaperProvider>
+<ScrollView>
 
-            
+</ScrollView>
+
             </PaperProvider>
     );
 }
