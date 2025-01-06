d19e334a0a055bec13f41a2cb557fcc1d87e497d
prathibhani <139643234+prathibhani@users.noreply.github.com>
2025-01-07 00:02:59 +0530

    View studentDetails in Profile(Name,Age,Gender,Contact info,Email,Phone no)

--- StudentCareApp/components/Profile.js
+++ StudentCareApp/components/Profile.js
@@ -11,6 +11,24 @@ export default  function Profile ({ route }){
             style={styles.image}
           />
         </View>
+        <View style={styles.studentInfoContainer}>
+          <Image
+            style={styles.profilePicContainer}
+            source={studentdata.profile_pic}
+          ></Image>
+          <Text style={styles.studentName}>Name:{studentdata.username}</Text>
+          <Text style={styles.studentDetails}>Age:{studentdata.age}</Text>
+          <Text style={styles.studentDetails}>Gender:{studentdata.gender}</Text>
+        </View>
+
+        <View style={styles.contactInfoContainer}>
+          <Text style={styles.contactTitle}>Contact Information</Text>
+          <Text style={styles.contactDetails}>Email: {studentdata.email}</Text>
+          <Text style={styles.contactDetails}>Phone: {studentdata.phone}</Text>
+          <Text style={styles.contactDetails}>
+            Address: {studentdata.address}
+          </Text>
+        </View>
 </ScrollView>
 </PaperProvider>
 
