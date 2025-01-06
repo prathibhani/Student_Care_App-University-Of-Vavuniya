d0bcefc8430520ea0a0ed0444827d1d8bd86fdc9
prathibhani <139643234+prathibhani@users.noreply.github.com>
2025-01-07 00:06:34 +0530

    Add the styles in Profile

--- StudentCareApp/components/Profile.js
+++ StudentCareApp/components/Profile.js
@@ -74,3 +74,95 @@ export default  function Profile ({ route }){
 
     );
 }
+const styles = StyleSheet.create({
+    header: {
+      width: "100%",
+      height: 100,
+      justifyContent: "center",
+      alignItems: "center",
+      backgroundColor: "#8b008b",
+    },
+    headerText: {
+      fontSize: 24,
+      color: "#fff",
+      backgroundColor: "#8b008b",
+      fontWeight: "bold",
+    },
+    image: {
+      width: "60%",
+      height: 100,
+      resizeMode: "contain",
+      marginTop: 5,
+    },
+    imagecontain: { alignItems: "center" },
+    loginText: {
+      fontSize: 40,
+      fontWeight: "bold",
+    },
+    studentInfoContainer: {
+      width: "100%",
+      alignItems: "center",
+      paddingHorizontal: 10,
+    },
+    profilePicContainer: {
+      width: 120,
+      height: 120,
+      borderRadius: 60,
+      overflow: "hidden",
+      marginBottom: 20,
+      borderWidth: 3,
+      borderColor: "#8b008b", // Matching border color
+    },
+    studentDetails: {
+      fontSize: 18,
+      color: "#555",
+      marginBottom: 5,
+      textAlign: "center",
+    },
+    studentName: {
+      fontSize: 28,
+      fontWeight: "bold",
+      color: "#333",
+      marginBottom: 10,
+    },
+    contactInfoContainer: {
+      marginTop: 20,
+      paddingHorizontal: 15,
+    },
+    contactTitle: {
+      fontSize: 20,
+      fontWeight: "bold",
+      marginBottom: 10,
+      color: "#8b008b",
+    },
+    contactDetails: {
+      fontSize: 10,
+      color: "#555",
+      marginBottom: 5,
+    },
+  
+    biologicalInfoContainer: {
+      marginTop: 20,
+      paddingHorizontal: 15,
+    },
+    biologicalTitle: {
+      fontSize: 20,
+      fontWeight: "bold",
+      marginBottom: 10,
+      color: "#8b008b",
+    },
+    biologicalDetails: {
+      fontSize: 10,
+      color: "#555",
+      marginBottom: 5,
+    },
+    buttonContainer: {
+      flexDirection: "row",
+      justifyContent: "space-between",
+      marginTop: 20,
+    },
+    button: {
+      marginHorizontal: 5,
+    },
+  });
+  
