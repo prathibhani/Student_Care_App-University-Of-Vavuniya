71f7964155bcef8ba89c9776cc6760702153007d
prathibhani <139643234+prathibhani@users.noreply.github.com>
2025-01-06 23:07:42 +0530

    created the PaperProvider component

---
+++ StudentCareApp/components/Login.js
@@ -0,0 +1,11 @@
+import { students } from "../data/StudentsDb";
+import { useNavigation } from "@react-navigation/native";
+
+export default function Login(){
+    return(
+        <PaperProvider>
+
+            
+            </PaperProvider>
+    );
+}
